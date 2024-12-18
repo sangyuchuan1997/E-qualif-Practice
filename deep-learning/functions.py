aee4d5d23fab97a1e54f10f6a88a1f29acf8d9ec
sangyuchuan1997 <97s08y25c@gmail.com>
2024-12-19 01:20:05 +0900

    Neural Network with Layers

--- deep-learning/functions.py
+++ deep-learning/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def step_function(x):
     y = x > 0
     return y.astype(np.int32)
@@ -18,7 +19,10 @@ def identity_function(x):
 
 
 def softmax(a):
-    a = a - np.max(a)  # オーバーフロー対策
+    if np.all(np.isnan(a)):
+        print("全てがNanになってしまいました.")
+        return a
+    a = a - np.nanmax(a)  # オーバーフロー対策
     exp_a = np.exp(a)
     sum_exp_a = np.sum(exp_a)
     return exp_a / sum_exp_a
@@ -33,7 +37,7 @@ def cross_entropy_error_simple(y, t):
     return -np.sum(t * np.log(y + delta))
 
 
-def cross_entropy_error_onehot(y : np.ndarray, t : np.ndarray):
+def cross_entropy_error_onehot(y: np.ndarray, t: np.ndarray):
     # ont hot 対応
     if y.ndim == 1:
         t = t.reshape(1, t.size)
@@ -42,10 +46,41 @@ def cross_entropy_error_onehot(y : np.ndarray, t : np.ndarray):
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
 
 
-def cross_entropy_error_label(y : np.ndarray, t : np.ndarray):
+def cross_entropy_error_label(y: np.ndarray, t: np.ndarray):
     # label 対応
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 値を元に戻す
+        it.iternext()
+
+    return grad
