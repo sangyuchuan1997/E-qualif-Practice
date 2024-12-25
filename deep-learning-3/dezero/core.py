ed74a26789a142ac9eca6eea5a97590c00aae697
sangyuchuan1997 <97s08y25c@gmail.com>
2024-12-26 01:16:12 +0900

    deep learning framework dezero

---
+++ deep-learning-3/dezero/core.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data: np.ndarray):
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func: 'Function'):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
---
+++ deep-learning-3/dezero/functions.py
@@ -0,0 +1,24 @@
+import numpy as np
+from core import Function
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+    
+def exp(x):
+    return Exp()(x)
